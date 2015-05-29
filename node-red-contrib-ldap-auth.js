52ece64fd65a7961cc8c615303673eb258e2889b
Ben Hardill <b.hardill@gmail.com>
2015-05-29 10:53:53 +0100

    Moved to a pure javascript ldap client so it will run on BlueMix/Cloud Foundry

--- node-red-contrib-ldap-auth.js
+++ node-red-contrib-ldap-auth.js
@@ -13,109 +13,99 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-
 var when = require("when");
 var mustache = require("mustache");
-var LDAP = require("LDAP");
- 
+var ldapjs = require("ldapjs");
+
+
 var filterTemplate = '';
 
-var searchOpts = {
-		scope: '',
-		attrs: ''
-	};
-
 var options = {
-		uri: '',
-		version: 3,
-		starttls: true,
-		reconnect: true
-	};
+};
+
+var searchOpts = {
+	scope: 'sub',
+	sizeLimit: 1
+};
 
 module.exports = {
 	setup: function(args) {
+		options.url = args.uri;
 		searchOpts.base = args.base;
 		filterTemplate = args.filterTemplate;
-		options.uri = args.uri
 
 		return this;
 	},
-    type: "credentials",
-    users: function(username) {
-        return when.promise(function(resolve) {
-        	var ldap = new LDAP(options);
-			ldap.open(function(err){
-				 // check user in ldap
-
+	type: "credentials",
+	users: function(username) {
+		return when.promise(function(resolve) {
+			var ldap = ldapjs.createClient(options);
+			var temp = {};
+			temp.username = username;
+			searchOpts.filter = mustache.render(filterTemplate, temp);
+			ldap.search(searchOpts.base,searchOpts,function(err, res){
+				var found = false;
 				if (err) {
-					console.log("error in open");
 					console.log(err);
+					resolve(null);
+					ldap.unbind();
+				} else {
+					res.on('searchEntry', function(entry){
+						found = true;
+						resolve({ username: username, permissions: "*" });
+						ldap.unbind();
+					});
+					res.on('end', function(status) {
+						if (!found) {
+							resolve(null);
+							ldap.unbind();
+						}
+					});
 				}
-
-				var temp = {};
-				temp.username = username;
-				searchOpts.filter = mustache.render(filterTemplate, temp);
-	            ldap.search(searchOpts, function(err,data){
-	            	if (err) {
-	            		console.log("err -" + err);
-	            		resolve(null);
-	            		ldap.close();
-	            	} else if (data) {
-	            		if (data.length == 1) {
-	            			resolve({ username: username, permissions: "*" });
-	            		} else {
-	            			resolve(null);
-	            		}
-	            		ldap.close();
-	            	}
-	            });
-	            
+				
 			});
-
-           
-        });
-    },
-    authenticate: function(username,password) {
-        return when.promise(function(resolve) {
-        	var ldap = new LDAP(options);
-			ldap.open(function(err){
-				// authenticate ldap
-				var temp = {};
-				temp.username = username;
-				searchOpts.filter = mustache.render(filterTemplate, temp);
-	            ldap.search(searchOpts, function(err,data){
-	            	if (err) {
-	            		console.log(err);
-	            		resolve(null);
-	            		ldap.close();
-	            	} else if (data) {
-	            		if (data.length == 1) {
-	            			var bindOptions = {
-								binddn: data[0].dn,
-								password: password
-							};
-	            			ldap.simpleBind(bindOptions, function(err){
-	            				if (err) {
-	            					resolve(null);
-	            				} else {
-	            					resolve({ username: username, permissions: "*" });
-	            				}
-	            				ldap.close();
-	            			});
-	            		} else {
-	            			resolve(null);
-	            			ldap.close();
-	            		}
-	            	}
-	            });
+		});
+	},
+	authenticate: function(username, password) {
+		return when.promise(function(resolve) {
+			var ldap = ldapjs.createClient(options);
+			var temp = {};
+			temp.username = username;
+			searchOpts.filter = mustache.render(filterTemplate, temp);
+			ldap.search(searchOpts.base,searchOpts,function(err, res){
+				var found = false;
+				if (err) {
+					console.log(err);
+					resolve(null);
+					ldap.unbind();
+				} else {
+					res.on('searchEntry', function(entry){
+						found = true;
+						ldap.bind(entry.dn, password, function(err){
+							if (!err) {
+								resolve({ username: username, permissions: "*" });
+								ldap.unbind();
+							} else {
+								resolve(null);
+								ldap.unbind();
+							}
+						});
+					});
+					res.on('end', function(status){
+						if (!found) {
+							resolve(null);
+							ldap.unbind();
+						}
+					});
+				}
 			});
-        });
-    },
-    default: function() {
+		});
+	},
+	default: function() {
         return when.promise(function(resolve) {
             // Resolve with the user object for the default user.
             // If no default user exists, resolve with null.
             resolve(null);
         });
     }
-}
+};
